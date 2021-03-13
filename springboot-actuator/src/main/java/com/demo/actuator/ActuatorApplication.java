d5a69a6a3550c47ba53252d4ae7eec3638814216
dean <719738494@qq.com>
2021-03-13 16:32:24 +0800

    add:springboot actuator

---
+++ springboot-actuator/src/main/java/com/demo/actuator/ActuatorApplication.java
@@ -0,0 +1,20 @@
+package com.demo.actuator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+/**
+ *
+ * @author Dean
+ * @date 2021-03-13
+ */
+@SpringBootApplication
+public class ActuatorApplication {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(ActuatorApplication.class, args);
+    }
+
+}
---
+++ springboot-actuator/src/test/java/com/demo/actuator/ActuatorApplicationTests.java
@@ -0,0 +1,17 @@
+package com.demo.actuator;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ *
+ * @author Dean
+ * @date 2021-03-13
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ActuatorApplicationTests {
+
+
+}
