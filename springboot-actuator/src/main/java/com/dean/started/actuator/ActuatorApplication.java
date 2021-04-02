8a33fde2027499741468405031e3975a17c28546
dean <719738494@qq.com>
2021-04-02 14:20:21 +0800

    update: package name

--- springboot-actuator/src/main/java/com/dean/started/actuator/ActuatorApplication.java
+++ springboot-actuator/src/main/java/com/dean/started/actuator/ActuatorApplication.java
@@ -1,4 +1,4 @@
-package com.demo.actuator;
+package com.dean.started.actuator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- springboot-actuator/src/test/java/com/dean/started/actuator/ActuatorApplicationTests.java
+++ springboot-actuator/src/test/java/com/dean/started/actuator/ActuatorApplicationTests.java
@@ -1,4 +1,4 @@
-package com.demo.actuator;
+package com.dean.started.actuator;
 
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
